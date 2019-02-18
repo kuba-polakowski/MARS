535a0160887e8bad7b204ef24ee6a28d83f46a81
Mac <mac@macs-macbook-pro.local>
2019-02-18 18:17:46 +0100

    Placeholder data (messages, events) and images

---
+++ MARS/Data/Events.swift
@@ -0,0 +1,14 @@
+//
+//  Events.swift
+//  MARS
+//
+//  Created by Mac on 2/17/19.
+//  Copyright © 2019 kubapolakowski. All rights reserved.
+//
+
+import Foundation
+
+let upcomingEvents: [Event] = [Event(title: "Supplies Drop", date: Date(), imageName: "rockets"),
+                               Event(title: "Dune Buggies on Valles Marineris", date: Date(), imageName: "buggy"),
+                               Event(title: "Solar Panel Maintenance", date: Date(), imageName: "panels")
+]
---
+++ MARS/Data/Messages.swift
@@ -0,0 +1,24 @@
+//
+//  Messages.swift
+//  MARS
+//
+//  Created by Mac on 2/13/19.
+//  Copyright © 2019 kubapolakowski. All rights reserved.
+//
+
+import Foundation
+
+let foodMessages: [Message] = [Message(author: "Homer", date: Date(), text: "3 Campari Cocktails You Don't Need a Gazillion Ingredients to Make"),
+                               Message(author: "Abe", date: Date(), text: "The Most-Used Knife In My Kitchen Only Cost $7"),
+                               Message(author: "Abe", date: Date(), text: "Most-Used Knife In My Kitchen Only Cost $7"),
+                               Message(author: "Abe", date: Date(), text: "The Knife In My Kitchen Only Cost $7"),
+                               Message(author: "Frink", date: Date(), text: "I Learned Some Lessons Spatchcocking a Chicken\nA flat bird is (usually) a good bird—except when you ignore the directions."),
+                               Message(author: "Moe", date: Date(), text: "This Lamb Tagine Is More Warming Than Your Fireplace App"),
+                               Message(author: "Moe", date: Date(), text: "Just so you know"),
+                               Message(author: "Peter", date: Date(), text: "At home, no one goes full molten chocolate cake. It requires making a cake, and a ganache, and doing all kinds of cake gymnastics to make sure the sponge is tender and cooked,"),
+                               Message(author: "Peter", date: Date(), text: "The ganache properly melted and silky."),
+                               Message(author: "Peter", date: Date(), text: "It’s a lot."),
+                               Message(author: "Abe", date: Date(), text: "The Most-Used Knife In My Kitchen Only Cost $7"),
+                               Message(author: "Abe", date: Date(), text: "The Most-Used Knife In My Kitchen Only Cost $7")
+    
+]

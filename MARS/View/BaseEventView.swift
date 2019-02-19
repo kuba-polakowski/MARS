c3bcab1b2bd8e69bc457b6d887baaaad1e273d6c
Mac <mac@macs-macbook-pro.local>
2019-02-19 20:34:10 +0100

    Base item view to be used in transition animation - temporary?

---
+++ MARS/View/BaseEventView.swift
@@ -0,0 +1,109 @@
+//
+//  BaseEventView.swift
+//  MARS
+//
+//  Created by Mac on 2/19/19.
+//  Copyright © 2019 kubapolakowski. All rights reserved.
+//
+
+import UIKit
+
+class BaseEventView: UIView {
+    
+    let thumbnailView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 25, weight: .semibold)
+        label.textColor = primaryFontColor
+        label.numberOfLines = 2
+        label.adjustsFontSizeToFitWidth = true
+        
+        return label
+    }()
+    
+    let dateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textColor = secondaryFontColor
+        
+        return label
+    }()
+    
+    var topThumbnailConstraint: NSLayoutConstraint!
+    var leadingThumbnailConstraint: NSLayoutConstraint!
+    var trailingThumbnailConstraint: NSLayoutConstraint!
+    var heightThumbnailConstraint: NSLayoutConstraint!
+    var dateLabelLeadingConstraint: NSLayoutConstraint!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = secondaryColor
+        layer.cornerRadius = 15
+        layer.masksToBounds = true
+        
+        addSubview(thumbnailView)
+        addSubview(titleLabel)
+        addSubview(dateLabel)
+        
+        topThumbnailConstraint = thumbnailView.topAnchor.constraint(equalTo: topAnchor)
+        heightThumbnailConstraint = thumbnailView.heightAnchor.constraint(equalToConstant: 220)
+        leadingThumbnailConstraint = thumbnailView.leadingAnchor.constraint(equalTo: leadingAnchor)
+        trailingThumbnailConstraint = thumbnailView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        
+        topThumbnailConstraint.isActive = true
+        leadingThumbnailConstraint.isActive = true
+        trailingThumbnailConstraint.isActive = true
+        heightThumbnailConstraint.isActive = true
+        
+        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: thumbnailView.bottomAnchor, constant: 10).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        
+        dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 5).isActive = true
+        dateLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+        
+        dateLabelLeadingConstraint = dateLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10)
+        dateLabelLeadingConstraint.isActive = true
+    }
+    
+    func animateForTransition() {
+        clipsToBounds = false
+        leadingThumbnailConstraint.constant = -15
+        trailingThumbnailConstraint.constant = 15
+        heightThumbnailConstraint.constant += 10
+        dateLabelLeadingConstraint.constant = frame.width - 110
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut, animations: { [unowned self] in
+            self.layoutIfNeeded()
+            self.titleLabel.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        })
+    }
+    
+    func setOriginalConstraints() {
+        clipsToBounds = true
+        leadingThumbnailConstraint.constant = 0
+        trailingThumbnailConstraint.constant = 0
+        heightThumbnailConstraint.constant -= 10
+        dateLabelLeadingConstraint.constant = 10
+        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseOut, animations: { [unowned self] in
+            self.layoutIfNeeded()
+            self.titleLabel.transform = .identity
+            self.backgroundColor = secondaryColor
+        })
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
